20e7d783ec883505961c0d4f294b20b97d5c2954
Davina Leong <davina.leong.sy@gmail.com>
2018-09-22 02:35:16 +0800

    Implemented simple config to toggle other packages

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    /**
+     * Front-end form validation
+     * Docs: http://parsleyjs.org
+     */
+    parsley: false,
+
+    /**
+     * Sort and search table data
+     * Docs: https://datatables.net/
+     */
+    dataTables: false,
+
+    /**
+     * Tempus Dominus datetime picker widget. This only includes its dependency, MomentJS
+     * Docs: https://momentjs.com/
+     * You can copy & paste this external links from the TD docs here:
+     * https://tempusdominus.github.io/bootstrap-4/
+     */
+    datetimePicker: false,
+
+    /**
+     * Code syntax highlighting.
+     * Docs: https://prismjs.com/
+     */
+    prism: true
+
+};
